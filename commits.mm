1ee99a41d67a82989df2efcf5e3b0bbf44225e72
Benjamin Behr <benjamin.behr@mindmatters.de>
2009-02-04 09:51:51 +0100

    first import

---
+++ commits.mm
@@ -0,0 +1,92 @@
+<map version="0.8.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1232709335245" ID="Freemind_Link_817686442" MODIFIED="1233306152348" TEXT="commit Messages">
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<node CREATED="1232709445563" HGAP="29" ID="_" MODIFIED="1233306002065" POSITION="right" TEXT="messages" VSHIFT="-23">
+<cloud/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1232711383525" ID="Freemind_Link_1968751997" MODIFIED="1232711386593" TEXT="Guidelines">
+<node CREATED="1232711387278" ID="Freemind_Link_955854586" MODIFIED="1232711388393" TEXT="Refer to the specs, whenever possible"/>
+<node CREATED="1232711433848" ID="Freemind_Link_786949257" MODIFIED="1233305623892" TEXT="Mind that the commit messages may be reviewed with or without the code." VSHIFT="24"/>
+<node CREATED="1232711571794" ID="Freemind_Link_1405743255" MODIFIED="1233305646277" TEXT="Fix:; New:; Enhancement:; Sorry:; &#x2013;" VSHIFT="21">
+<node CREATED="1233305646842" ID="Freemind_Link_1205064366" MODIFIED="1233305647789" TEXT="what did you do?"/>
+</node>
+<node CREATED="1232711630837" HGAP="23" ID="Freemind_Link_1662304596" MODIFIED="1233305650286" TEXT="Reveal your intentions, don&#x2019;t describe what the code does. Tell why it does it." VSHIFT="24"/>
+<node CREATED="1232711763109" ID="Freemind_Link_1647094637" MODIFIED="1233306744382" TEXT="Leave insights, skip profanity." VSHIFT="21">
+<node CREATED="1232711781406" ID="Freemind_Link_579487509" MODIFIED="1232711782558" TEXT="&#x201c;Hum, this does not look right; Ilya, please see if you have the time.&#x201d;"/>
+</node>
+<node CREATED="1232711802728" HGAP="21" ID="Freemind_Link_666400520" MODIFIED="1233305626020" TEXT="Stay concise, be up to the point." VSHIFT="16">
+<node CREATED="1232711817985" ID="Freemind_Link_1136246567" MODIFIED="1232711819128" TEXT="A long commit message is usually a bad sign for too much stuff done in a single commit."/>
+<node CREATED="1232711835994" HGAP="23" ID="Freemind_Link_705520053" MODIFIED="1233305707737" TEXT="gut: 1-2 S&#xe4;tze" VSHIFT="13"/>
+</node>
+</node>
+<node CREATED="1232709734972" HGAP="32" ID="Freemind_Link_1068303418" MODIFIED="1233305811943" TEXT="Aufbau" VSHIFT="33">
+<node CREATED="1232709660777" HGAP="21" ID="Freemind_Link_1279260920" MODIFIED="1232709741044" TEXT="subject line" VSHIFT="-8">
+<node CREATED="1232709918820" ID="Freemind_Link_871582911" MODIFIED="1232709948978" TEXT="z.B. Ticket-Titel (mit Nr), Story-Titel"/>
+<node CREATED="1232709986656" ID="Freemind_Link_1536322074" MODIFIED="1232709991580" TEXT="Start mit" VSHIFT="10">
+<node CREATED="1232711497837" ID="Freemind_Link_1745426813" MODIFIED="1232711501969" TEXT="Variante 1">
+<node CREATED="1232709995273" ID="Freemind_Link_1707172583" MODIFIED="1232710000748" TEXT="&quot;Added ...&quot;">
+<node CREATED="1232710006361" ID="Freemind_Link_1898843335" MODIFIED="1232710008860" TEXT="-&gt; new feature"/>
+</node>
+<node CREATED="1232710014337" ID="Freemind_Link_908375439" MODIFIED="1232710023189" TEXT="&quot;Fixed ...&quot;">
+<node CREATED="1232710024178" ID="Freemind_Link_1741315205" MODIFIED="1232710027558" TEXT="-&gt; Bug fix">
+<node CREATED="1232710180810" ID="Freemind_Link_952355005" MODIFIED="1232710182094" TEXT="Do not to describe what was really wrong, just say what was fixed"/>
+</node>
+</node>
+<node CREATED="1232710041899" ID="Freemind_Link_1533896731" MODIFIED="1232710066912" TEXT="&quot;Changed ...&quot;">
+<node CREATED="1232710067700" ID="Freemind_Link_1551887853" MODIFIED="1232710203847" TEXT="-&gt; Task (Refactoring)"/>
+</node>
+<node CREATED="1232710081477" ID="Freemind_Link_34215868" MODIFIED="1232710087673" TEXT="&quot;Updated ...&quot;">
+<node CREATED="1232710088069" ID="Freemind_Link_650136353" MODIFIED="1232710100538" TEXT="-&gt; task, especially used for required changes due to changes in 3rd party code"/>
+</node>
+</node>
+<node CREATED="1232711503509" ID="Freemind_Link_1264768241" MODIFIED="1233305760676" TEXT="Variante 2" VSHIFT="27">
+<node CREATED="1232711539824" ID="Freemind_Link_1592560111" MODIFIED="1232711541271" TEXT="Fix: &#x2013; closing bug"/>
+<node CREATED="1232711545928" ID="Freemind_Link_987752775" MODIFIED="1232711548368" TEXT="New: &#x2013; implemented something new"/>
+<node CREATED="1232711554049" ID="Freemind_Link_885295479" MODIFIED="1232711555409" TEXT="Enhancement: &#x2013; we&#x2019;re making it better."/>
+<node CREATED="1232711562009" ID="Freemind_Link_972262810" MODIFIED="1232711563353" TEXT="Oops: &#x2013; Even the best make mistakes, commits containing typos or plain non-working code. Show your shame!"/>
+</node>
+</node>
+</node>
+<node CREATED="1232709741804" ID="Freemind_Link_1627168326" MODIFIED="1232709744496" TEXT="Leerzeile"/>
+<node CREATED="1232709745444" ID="Freemind_Link_10158134" MODIFIED="1233305545135" TEXT="Body" VSHIFT="14"/>
+<node CREATED="1232709750844" ID="Freemind_Link_1228913617" MODIFIED="1233305746347" TEXT="=&gt; Automatisches Mapping in Email" VSHIFT="16"/>
+</node>
+<node CREATED="1232710325356" FOLDED="true" HGAP="21" ID="Freemind_Link_569296230" MODIFIED="1233305813488" TEXT="Beispiele" VSHIFT="39">
+<node CREATED="1232710719422" ID="Freemind_Link_803969480" MODIFIED="1232710729743" TEXT="simple" VSHIFT="18">
+<node CREATED="1232710330436" ID="Freemind_Link_90810658" MODIFIED="1232710348681" TEXT="#123456: Added project release tracking."/>
+<node CREATED="1232710561140" ID="Freemind_Link_1377423624" MODIFIED="1232710566144" TEXT="#46746 by Matt: Fixed inconsistent encoding of path aliases." VSHIFT="6"/>
+</node>
+<node CREATED="1232710683852" ID="Freemind_Link_530373881" MODIFIED="1232710687567" TEXT="Extended commit message, including non-technical explanation:" VSHIFT="12">
+<node CREATED="1232710696076" ID="Freemind_Link_1950876622" MODIFIED="1232710713011" TEXT="#46746 by Matt: Fixed inconsistent encoding of path aliases. &#xa;Fixes broken URLs on profile pages."/>
+</node>
+<node CREATED="1232710744400" ID="Freemind_Link_1773931151" MODIFIED="1232710774493" TEXT="long" VSHIFT="20">
+<node CREATED="1232710766017" ID="Freemind_Link_1931382096" MODIFIED="1232710772527" TEXT="#[issue number] by [username]: [Short summary of the change].&#xa;&#xa;A bug in [filename(s)] caused [short description of the problem]. This commit&#xa;solves this by:&#xa;* [first solution or change]&#xa;* [second solution or change]&#xa;* [third solution or change]&#xa;* Adding $foo as parameter to the API call module_function() in bar.&#xa;&#xa;[Optional short note on the effects for code calling your APIs or functions,&#xa;or users using your modules.]"/>
+</node>
+</node>
+<node CREATED="1232711078829" ID="Freemind_Link_1564775062" MODIFIED="1232711195913" TEXT="Zitate" VSHIFT="14">
+<node CREATED="1232711101111" FOLDED="true" HGAP="22" ID="Freemind_Link_812135590" MODIFIED="1232711194776" TEXT="http://www.wildbit.com/blog/2008/11/11/the-importance-of-commit-messages/" VSHIFT="1">
+<node CREATED="1232711083765" ID="Freemind_Link_1752700911" MODIFIED="1232711093114" TEXT="&quot;the commit messages are (one of) your primary means of communication with the rest of the team&quot;">
+<node CREATED="1233305567532" ID="Freemind_Link_1863508189" MODIFIED="1233305578085" TEXT="Stand-Up?"/>
+</node>
+<node CREATED="1232711178835" HGAP="21" ID="Freemind_Link_1958710588" MODIFIED="1233305597874" TEXT="&quot;thinking in terms of your next commit helps you stay focused on the task at hand.&quot;" VSHIFT="6">
+<node CREATED="1232711234438" ID="Freemind_Link_1316838969" MODIFIED="1232711235474" TEXT="several non-related changes">
+<node CREATED="1232711243927" ID="Freemind_Link_1355278296" MODIFIED="1232711245134" TEXT="hard time describing what I did"/>
+</node>
+</node>
+<node CREATED="1232711268720" HGAP="21" ID="Freemind_Link_701537731" MODIFIED="1233305596762" TEXT="&quot;The commit message is like the goal for your coding session.&quot;" VSHIFT="11"/>
+</node>
+</node>
+</node>
+<node CREATED="1232709512685" FOLDED="true" HGAP="43" ID="Freemind_Link_1312559178" MODIFIED="1233306000505" POSITION="left" TEXT="Referenz">
+<cloud/>
+<node CREATED="1232710820662" ID="Freemind_Link_1379358404" MODIFIED="1232710831433" TEXT="http://drupal.org/node/52287" VSHIFT="-1"/>
+<node CREATED="1232709518493" ID="Freemind_Link_1277782549" MODIFIED="1232710832329" TEXT="http://www.tpope.net/node/106" VSHIFT="11"/>
+</node>
+<node CREATED="1233305852086" FOLDED="true" HGAP="29" ID="Freemind_Link_1319977665" MODIFIED="1233306004145" POSITION="right" TEXT="sonstiges" VSHIFT="27">
+<cloud/>
+<node CREATED="1232710454805" ID="Freemind_Link_1356104085" MODIFIED="1233305865610" TEXT="CHANGELOG.txt?" VSHIFT="6"/>
+<node CREATED="1232709864657" HGAP="21" ID="Freemind_Link_631492950" MODIFIED="1233305866378" TEXT="RSS-Feed?" VSHIFT="10"/>
+</node>
+</node>
+</map>
